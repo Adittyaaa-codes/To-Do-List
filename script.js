8a0937def4d250a2f3587132efc82961af1ff947
Adittyaaa-codes <aditya.mallick.1512@gmail.com>
2025-07-05 09:22:22 +0530

    js changes

--- script.js
+++ script.js
@@ -1,138 +1,176 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function () {
     const input = document.getElementById("input");
     const add = document.getElementById("add");
     const lists = document.getElementById("list");
     const taskHeader = document.getElementById("task-header");
 
     function saveData() {
-        localStorage.setItem("data", list.innerHTML);
+        localStorage.setItem("data", lists.innerHTML);
     }
 
     function updateTaskCount() {
-        const total = lists.children.length;
-        const completed = lists.querySelectorAll("input[type='checkbox']:checked").length;
-
-        taskHeader.textContent = `Task Completed - ${completed}/${total}`;
+        let total = lists.querySelectorAll("li").length;
+        let completed = lists.querySelectorAll("input[type='checkbox']:checked").length;
 
         if (total === 0) {
             taskHeader.textContent = "Enter your tasks!!";
             taskHeader.style.textAlign = "center";
             taskHeader.style.color = "white";
-            return;
-        }
-
-        const percentage = (completed / total) * 100;
-
-        if (percentage < 50) {
-            taskHeader.style.color = "red";
-        } else if (completed === total) {
-            taskHeader.style.color = "green";
         } else {
-            taskHeader.style.color = "orange";
+            taskHeader.textContent = "Task Completed - " + completed + "/" + total;
+            let percent = (completed / total) * 100;
+            if (percent < 50) {
+                taskHeader.style.color = "red";
+            } else if (percent === 100) {
+                taskHeader.style.color = "green";
+            } else {
+                taskHeader.style.color = "orange";
+            }
         }
-        saveData();
     }
 
-    updateTaskCount();
-
     function addTask() {
-        if (input.value === "") {
+        let task = input.value.trim();
+        if (task === "") {
             alert("You must write something to add!!");
             return;
         }
 
-        const li = document.createElement("li");
-
+        let li = document.createElement("li");
         li.innerHTML = `
-        <div class="in-list">
-        <div class="il-left">
-        <input type="checkbox" name="" id="checkbox">${input.value} 
-                        </div>
-                        <div class="il-right">
-                            <button class="il-button edit" ><i class="fa-solid fa-pen" style="color:green; font-size:15px; padding:5px; border-radius:50%;"></i></button>
-                            <button class="il-button trash"><i class="fa-solid fa-trash-can" style="color:red; font-size:15px; padding:5px; border-radius:50%;"></i></button>
-                        </div>
-                    </div>`;
+            <div class="in-list">
+                <div class="il-left">
+                    <input type="checkbox">
+                    <span class="task-text">${task}</span>
+                </div>
+                <div class="il-right">
+                    <button class="il-button edit">
+                        <i class="fa-solid fa-pen" style="color:green; font-size:15px; padding:5px; border-radius:50%;"></i>
+                    </button>
+                    <button class="il-button trash">
+                        <i class="fa-solid fa-trash-can" style="color:red; font-size:15px; padding:5px; border-radius:50%;"></i>
+                    </button>
+                </div>
+            </div>
+        `;
+
+        let checkbox = li.querySelector("input[type='checkbox']");
+        checkbox.addEventListener("change", function () {
+            updateTaskCount();
+            saveData();
+        });
+
+        let editBtn = li.querySelector(".edit");
+        let trashBtn = li.querySelector(".trash");
+
+        editBtn.addEventListener("click", function () {
+            let span = li.querySelector(".task-text");
+            let oldText = span.textContent;
+            let inputEdit = document.createElement("input");
+            inputEdit.type = "text";
+            inputEdit.value = oldText;
+            span.replaceWith(inputEdit);
+            inputEdit.focus();
+
+            inputEdit.addEventListener("keypress", function (e) {
+                if (e.key === "Enter") {
+                    let newText = inputEdit.value.trim();
+                    if (newText !== "") {
+                        let newSpan = document.createElement("span");
+                        newSpan.className = "task-text";
+                        newSpan.textContent = newText;
+                        inputEdit.replaceWith(newSpan);
+                        saveData();
+                    }
+                }
+            });
+
+            inputEdit.addEventListener("blur", function () {
+                let newSpan = document.createElement("span");
+                newSpan.className = "task-text";
+                newSpan.textContent = inputEdit.value.trim() || oldText;
+                inputEdit.replaceWith(newSpan);
+                saveData();
+            });
+        });
+
+        trashBtn.addEventListener("click", function () {
+            li.remove();
+            updateTaskCount();
+            saveData();
+        });
 
         lists.appendChild(li);
         input.value = "";
-
-        const checkbox = li.querySelector("input[type='checkbox']");
-        updateTaskCount();
-        checkbox.addEventListener("change", () => {
-            updateTaskCount();
-        });
-
-
-        const del = li.querySelector(".trash");
-        const edit = li.querySelector(".edit");
-
-
-        del.addEventListener("click", () => {
-            li.remove();
-            updateTaskCount();
-        });
-
-
-        edit.addEventListener("click", () => {
-            li.innerHTML = `<input type="text" id="edit-input" value="${li.innerText.trim()}" />`;
-
-            const inputBox = li.querySelector("#edit-input");
-
-            inputBox.addEventListener("keypress", (e) => {
-                if (e.key === "Enter") {
-                    const newText = inputBox.value.trim();
-                    li.innerHTML = `<div class="in-list">
-        <div class="il-left">
-        <input type="checkbox" name="" id="checkbox">${newText} 
-                        </div>
-                        <div class="il-right">
-                        <button class="il-button trash"><i class="fa-solid fa-trash-can" style="color:red; font-size:15px; padding:5px; border-radius:50%;"></i></button>
-                        </div>
-                    </div>`;
-                    updateTaskCount();
-                    const del = li.querySelector(".trash");
-                    del.addEventListener("click", () => {
-                        li.remove();
-                        updateTaskCount();
-                    });
-                }
-            });
-        });
         updateTaskCount();
         saveData();
     }
 
-
-
-    add.addEventListener("click", () => {
-        addTask();
-    });
-    input.addEventListener("keypress", (e) => {
-        if (e.key === "Enter") {
-            addTask();
-        }
-    });
-
-
     function loadData() {
-        const data = localStorage.getItem("data");
+        let data = localStorage.getItem("data");
         if (data) {
             lists.innerHTML = data;
-            lists.querySelectorAll("li").forEach(li => {
-                const checkbox = li.querySelector("input[type='checkbox']");
-                checkbox.addEventListener("change", () => {
+
+            lists.querySelectorAll("li").forEach(function (li) {
+                let checkbox = li.querySelector("input[type='checkbox']");
+                let editBtn = li.querySelector(".edit");
+                let trashBtn = li.querySelector(".trash");
+
+                checkbox.addEventListener("change", function () {
+                    updateTaskCount();
+                    saveData();
+                });
+
+                editBtn.addEventListener("click", function () {
+                    let span = li.querySelector(".task-text");
+                    let oldText = span.textContent;
+                    let inputEdit = document.createElement("input");
+                    inputEdit.type = "text";
+                    inputEdit.value = oldText;
+                    span.replaceWith(inputEdit);
+                    inputEdit.focus();
+
+                    inputEdit.addEventListener("keypress", function (e) {
+                        if (e.key === "Enter") {
+                            let newText = inputEdit.value.trim();
+                            if (newText !== "") {
+                                let newSpan = document.createElement("span");
+                                newSpan.className = "task-text";
+                                newSpan.textContent = newText;
+                                inputEdit.replaceWith(newSpan);
+                                saveData();
+                            }
+                        }
+                    });
+
+                    inputEdit.addEventListener("blur", function () {
+                        let newSpan = document.createElement("span");
+                        newSpan.className = "task-text";
+                        newSpan.textContent = inputEdit.value.trim() || oldText;
+                        inputEdit.replaceWith(newSpan);
+                        saveData();
+                    });
+                });
+
+                trashBtn.addEventListener("click", function () {
+                    li.remove();
                     updateTaskCount();
                     saveData();
                 });
             });
         }
-
-    }
-        function saveData() {
-        localStorage.setItem("data", list.innerHTML);
     }
 
-        updateTaskCount();
-        loadData();
-})
+    add.addEventListener("click", function () {
+        addTask();
+    });
+
+    input.addEventListener("keypress", function (e) {
+        if (e.key === "Enter") {
+            addTask();
+        }
+    });
+
+    loadData();
+    updateTaskCount();
+});
